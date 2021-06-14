8c8ed82fe7be4332759ee40472fa5e8c59656f27
Anand Mahesh <manand881@gmail.com>
2021-06-14 10:06:06 +0530

    cleaning code

--- sort.py
+++ sort.py
@@ -1,9 +1,13 @@
 import json
 import plotly.graph_objects as go
 
+# loading file which contains json data got from recievedata.py
+
 with open('data/1623583088.8387012 marketdata.json') as f:
     data = json.load(f)
     pairs = data["Time Series (5min)"].items()
+
+# initializing variables for usage
 
 timestampslist = list()
 openlist = list()
@@ -18,12 +22,16 @@ truerangelist = list()
 multiplier = 3.3
 days = 7
 atrlist = [0]*days
-upperband = [0]*days
-lowerband = [0]*days
-supertrend=[0]*days
+upperband = [0]*100
+lowerband = [0]*100
+supertrend=[0]*100
+
+# copying all the time stands to a list for sorting
 
 for key, value in pairs:
     timestampslist.append(key)
+
+# sorting json data by time
 
 timestampslist.sort()
 for x in timestampslist:
@@ -31,7 +39,7 @@ for x in timestampslist:
     openlist.append(data["Time Series (5min)"][x]["1. open"])
     highlist.append(data["Time Series (5min)"][x]["2. high"])
     lowlist.append(data["Time Series (5min)"][x]["3. low"])
-    closelist.append(data["Time Series (5min)"][x]["4. close"])
+    closelist.append(float(data["Time Series (5min)"][x]["4. close"]))
     volumelist.append(data["Time Series (5min)"][x]["5. volume"])
     upperbandbasic.append(((float((data["Time Series (5min)"][x]["2. high"]))-float(
         (data["Time Series (5min)"][x]["3. low"])))/2))
@@ -44,6 +52,9 @@ for x in timestampslist:
     dummylist.append(float(data["Time Series (5min)"][x]["3. low"]) -
                      float(data["Time Series (5min)"][x]["4. close"]))
     truerangelist.append(max(dummylist))
+
+# calculating ATR
+
 z = 0
 for x in range(days, len(truerangelist)):
     sum = 0
@@ -51,6 +62,8 @@ for x in range(days, len(truerangelist)):
         sum += truerangelist[y]
     z += 1
     atrlist.append(sum*(1/days))
+
+# calculating upperband basic and lowerband basic
 
 for x in range(0, len(lowerbandbasic)):
     try:
@@ -60,7 +73,9 @@ for x in range(0, len(lowerbandbasic)):
         lowerbandbasic[x] = 0
         upperbandbasic[x] = 0
 
-for x in range(days, len(lowerband)):
+# calculating upperband and lower band
+
+for x in range(days, len(lowerbandbasic)):
     if ((upperbandbasic[x] < upperband[x-1]) or (closelist[x-1] > upperband[x-1])):
         upperband[x] = upperbandbasic[x]
     else:
@@ -70,6 +85,8 @@ for x in range(days, len(lowerband)):
         lowerband[x] = lowerbandbasic[x]
     else:
         lowerband[x] = lowerband[x-1]
+
+# calculating supertrend
 
 for x in range(days, len(lowerband)):
     if((supertrend[x-1]==upperband[x-1])and(closelist[x]<upperband[x])):
@@ -81,15 +98,21 @@ for x in range(days, len(lowerband)):
     if((supertrend[x-1]==lowerband[x-1])and(closelist[x]<lowerband[x])):
         supertrend[x]=upperband[x]
 
+# passing high low open and close data to plotly api to plot candlestick
+
 fig = go.Figure(data=[go.Candlestick(x=timestampslist,
                                      open=openlist,
                                      high=highlist,
                                      low=lowlist,
                                      close=closelist)])
 
+# adding title to graph from json values
+
 fig.update_layout(
     title=data["Meta Data"]["1. Information"] +
     " for "+data["Meta Data"]["2. Symbol"],
 )
+
+# displaying graph in localhost
 
 fig.show()
