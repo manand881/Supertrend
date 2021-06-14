1c709e1dd5a644524abed38ce88b33ecdd174322
Anand Mahesh <manand881@gmail.com>
2021-06-14 10:09:59 +0530

    cleaning code

--- recievedata.py
+++ recievedata.py
@@ -2,19 +2,27 @@ import requests
 import json
 import time
 import os
+
+# trying to get response from api. using try catch in case of connectivity issues
+
 try:
     x = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo')
     json_obj = json.loads(x.text)
     vartime=time.time()
-    # timestamp={"timestamp":vartime}
-    # timestamp.update(json_obj)
     try:
         os.mkdir('data')
     except:
         pass
     with open('data/'+str(vartime)+' marketdata.json', 'w+') as outfile:
+        
+        # storing response to file
+
         json.dump(json_obj, outfile)
+
 except Exception as e:
+        
+        # recording exceptions 
+        
         f=open('Exception marketdata.txt', 'a+')
         f.writelines(e)
         f.close()
